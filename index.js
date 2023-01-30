5565eb12442fd8af13615723180ad695ad8c757b
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-01-30 09:23:49 +0100

    adjust cli

--- index.js
+++ index.js
@@ -1,45 +1,86 @@
 const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
+const prompts = require("prompts");
 
-const PATH = path.join(__dirname, "actual");
-const UI_PATH = path.join(__dirname, "ui");
+const DIRNAME = path.join(__dirname);
+const SNAPSHOT_PATH = path.join(DIRNAME, "snapshots");
 
-if (fs.existsSync(UI_PATH)) {
-  fs.rmSync(UI_PATH, { recursive: true, force: true });
-}
-fs.mkdirSync(UI_PATH);
+(async () => {
+  const snapshots = readSnapshotFolders();
 
-glob(path.join(PATH, "**/*.png"), {}, function (err, files) {
-  const failedFiles = files
-    .filter((f) => !f.includes("/visual/"))
-    .map((f) => path.dirname(f).split(path.sep).pop());
+  const response = await prompts({
+    type: "select",
+    name: "value",
+    message: "Pick a snapshot",
+    choices: snapshots,
+    initial: 0,
+  });
 
-  const testFiles = files.filter((f) => f.includes("/visual/"));
+  const chosenSnapshotFolder = snapshots[response.value].title;
+  const snapshotFolderPath = path.join(SNAPSHOT_PATH, chosenSnapshotFolder);
+  const uiFolderPath = path.join(snapshotFolderPath, "ui");
 
-  for (let index = 0; index < testFiles.length; index++) {
-    const file = testFiles[index];
+  createFolder(uiFolderPath);
 
-    const parts = file.split("/visual/")[1].split(".visual.cy.ts/");
-    const componentName = parts[0];
-    if (failedFiles.indexOf(`${componentName}.visual.cy.ts`) >= 0) {
-        const folderName = path.dirname(file).split(path.sep).pop();
+  const images = await loadBaseImages(snapshotFolderPath);
 
-        const uiFolderPath = path.join(UI_PATH, folderName);
+  for (let index = 0; index < images.length; index++) {
+    const image = images[index];
+    const testFolder = path.join(uiFolderPath, folderName(image));
+    createFolder(testFolder);
 
-        if (!fs.existsSync(uiFolderPath)) {
-          fs.mkdirSync(uiFolderPath);
-        }
-
-        fs.writeFileSync(
-          path.join(uiFolderPath, `${parts[1]}.html`),
-          htmlTemplate(componentName, parts[1].replace("-actual.png", ""))
-        );
-    }
+    fs.writeFileSync(
+      path.join(testFolder, `${fileName(image)}.html`),
+      htmlTemplate(snapshotFolderPath, image)
+    );
   }
-});
+})();
 
-function htmlTemplate(componentName, fileName) {
+function readSnapshotFolders() {
+  return fs
+    .readdirSync(SNAPSHOT_PATH)
+    .filter((d) => d.startsWith("cypress"))
+    .reverse()
+    .map((d) => ({
+      title: d,
+    }));
+}
+
+function createFolder(folderPath) {
+  if (fs.existsSync(folderPath)) {
+    fs.rmSync(folderPath, { recursive: true, force: true });
+  }
+  fs.mkdirSync(folderPath);
+}
+
+function folderName(file) {
+  return path.dirname(file).split(path.sep).pop();
+}
+
+function fileName(file) {
+  return file.split(path.sep).pop().replace("-actual.png", "");
+}
+
+function loadBaseImages(folderPath) {
+  return new Promise((resolve) => {
+    glob(path.join(folderPath, "actual/**/*.png"), {}, function (_err, files) {
+      const failedFiles = files
+        .filter((f) => !f.includes("/visual/"))
+        .map((f) => path.dirname(f).split(path.sep).pop());
+
+      const testFiles = files.filter((f) => f.includes("/visual/"));
+
+      resolve(testFiles.filter((f) => failedFiles.includes(folderName(f))));
+    });
+  });
+}
+
+function htmlTemplate(snapshotFolder, file) {
+  const imagePath = `../..${file
+    .replace(snapshotFolder, "")
+    .replace("-actual.png", "")}`;
+  console.log(imagePath);
   return `<html>
     <style>
         img {
@@ -52,18 +93,24 @@ function htmlTemplate(componentName, fileName) {
             <div style="display: flex; flex: 1; min-height: 50vh; background-color: #e8e8e8">
                 <div style="flex: 1; display: flex; justify-content: center;">
                     <div style="padding: 16px;">
-                        <img src="../../base/visual/${componentName}.visual.cy.ts/${fileName}-base.png" alt="base">
+                        <img src="${imagePath.replace(
+                          "actual",
+                          "base"
+                        )}-base.png" alt="base">
                     </div>
                 </div>
                 <div style="flex: 1; display: flex; justify-content: center;">
                     <div style="padding: 16px;">
-                        <img src="../../actual/visual/${componentName}.visual.cy.ts/${fileName}-actual.png" alt="actual">
+                      <img src="${imagePath}-actual.png" alt="actual">
                     </div>
                 </div>
             </div>
             <div style="display: flex; justify-content: center; min-height: 50vh; background-color: #ffaca6;">
-                <div style="padding: 16px;">
-                    <img src="../../diff/visual/${componentName}.visual.cy.ts/${fileName}-diff.png" alt="diff">
+              <div style="padding: 16px;">
+                <img src="${imagePath.replace(
+                  "actual",
+                  "diff"
+                )}-diff.png" alt="diff">                
                 </div>
             </div>
         </div>
